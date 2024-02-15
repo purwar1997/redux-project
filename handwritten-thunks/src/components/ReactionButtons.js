c71fbd5cea0f05efef7ed6046916240f5c23cf51
purwar1997 <shubhampurwar511@gmail.com>
2024-02-15 12:20:44 +0530

    Add custom CSS to tailwind input file

--- handwritten-thunks/src/components/ReactionButtons.js
+++ handwritten-thunks/src/components/ReactionButtons.js
@@ -10,7 +10,7 @@ const ReactionButtons = ({ post }) => {
   return (
     <div className='space-x-3'>
       {Object.entries(reactionEmojis).map(([emojiName, emojiType]) => (
-        <button className='border border-gray-400 px-2.5 py-0.5 rounded space-x-2'>
+        <button className='border border-gray-400 px-2.5 py-0.5 rounded space-x-2' key={emojiName}>
           <span>{emojiType}</span>
           <span>{post.reactions[emojiName]}</span>
         </button>
--- handwritten-thunks/src/pages/AddPostForm.js
+++ handwritten-thunks/src/pages/AddPostForm.js
@@ -1,5 +1,61 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
 const AddPostForm = () => {
-  return <section>AddPostForm</section>;
+  const [postTitle, setPostTitle] = useState('');
+  const [postContent, setPostContent] = useState('');
+  const [postAuthor, setPostAuthor] = useState('');
+
+  const users = useSelector(state => state.users);
+
+  return (
+    <section>
+      <h2 className='text-2xl'>Add new post</h2>
+
+      <form className='mt-8 space-y-4'>
+        <div className='form-control'>
+          <label htmlFor='title'>Post title</label>
+
+          <input
+            type='text'
+            id='title'
+            value={postTitle}
+            onChange={e => setPostTitle(e.target.value)}
+          />
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor='content'>Post content</label>
+
+          <textarea
+            id='content'
+            value={postContent}
+            onChange={e => setPostContent(e.target.value)}
+          />
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor='author'>Post author</label>
+
+          <select id='author' value={postAuthor} onChange={e => setPostAuthor(e.target.value)}>
+            <option value='' disabled hidden>
+              -- Please select an option --
+            </option>
+
+            {users.map(user => (
+              <option key={user.id} value={user.name}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <button className='ml-32 action-btn' type='button' onClick=''>
+          Save
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default AddPostForm;
