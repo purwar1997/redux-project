787e7978e69b6784eb6b41147ac06897dbca07a5
purwar1997 <shubhampurwar511@gmail.com>
2024-02-17 22:25:34 +0530

    Create mock model definitions using factory function

--- handwritten-thunks/src/api/server.js
+++ handwritten-thunks/src/api/server.js
@@ -1,5 +1,6 @@
 import seedrandom from 'seedrandom';
 import { faker } from '@faker-js/faker';
+import { factory, primaryKey, oneOf, manyOf } from '@mswjs/data';
 
 let rng = seedrandom();
 
@@ -19,3 +20,29 @@ if (useSeededRNG) {
   rng = seedrandom(randomSeedString);
   faker.seed(seedDate.getItem());
 }
+
+const db = factory({
+  user: {
+    id: primaryKey(faker.datatype.uuid),
+    name: String,
+    username: String,
+    posts: manyOf('post'),
+  },
+  post: {
+    id: primaryKey(faker.datatype.uuid),
+    title: String,
+    content: String,
+    date: String,
+    user: oneOf('user'),
+    reactions: oneOf('reaction'),
+  },
+  reaction: {
+    id: primaryKey(faker.datatype.uuid),
+    thumbsUp: Number,
+    wow: Number,
+    heart: Number,
+    rocket: Number,
+    coffee: Number,
+    post: oneOf('post'),
+  },
+});
